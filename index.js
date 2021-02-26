28f6b9645cba1363dab750a989e76915902130e1
call-me-bk <bkkarthik@pesu.pes.edu>
2021-02-26 14:57:35 +0530

    Add index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function showcontact(){
+    $("#contact_container").css("display","inherit");
+    $("#contact_container").addClass("animated slideInUp");
+    setTimeout(function(){
+        $("#contact_container").removeClass("animated slideInUp");
+    },800);
+}
+function closecontact(){
+    $("#contact_container").addClass("animated slideOutDown");
+    setTimeout(function(){
+        $("#contact_container").removeClass("animated slideOutDown");
+        $("#contact_container").css("display","none");
+    },800);
+}
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+      $("#box").css("display","none");
+      $("#contact").removeClass("animated fadeIn");
+    },1000);
+},1500);
